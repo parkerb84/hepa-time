9626cf414928ee943ce8ca837613b0e7b1fe9c2c
parkerb84 <fenratsu@gmail.com>
2018-10-30 14:34:43 -0400

    Added in registry subscription to the hepa-time service

--- bin/run.js
+++ bin/run.js
@@ -1,12 +1,27 @@
 'use strict';
 
 require('dotenv').config();
+const request = require('superagent');
 const service = require('../server/service');
 const http = require('http');
 
 const server = http.createServer(service);
-server.listen(3010);
+server.listen();
 
 server.on('listening', function() {
     console.log(`Hepa-Time is listening on ${server.address().port} in ${service.get('env')} mode.`);
+
+    const announce = () => {
+        request.put(`http://localhost:3000/service/time/${server.address().port}`, (err, res) => {
+            if(err) {
+                console.log(err);
+                console.log("Error in connecting to Hepa");
+                return;
+            }
+            console.log(res.body);
+        });
+    };
+
+    announce();
+    setInterval(announce, 15*1000);
 });
